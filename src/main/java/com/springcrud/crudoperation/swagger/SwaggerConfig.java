30d790cc17b4439011154b4839f99820d1fc99a6
Prem-hepl <premkumar.s@hepl.com>
2024-10-22 14:50:18 +0530

    swagger

--- src/main/java/com/springcrud/crudoperation/swagger/SwaggerConfig.java
+++ src/main/java/com/springcrud/crudoperation/swagger/SwaggerConfig.java
@@ -1,4 +1,30 @@
 package com.springcrud.crudoperation.swagger;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+
+@Configuration
+@EnableWebMvc
 public class SwaggerConfig {
+
+    @Bean
+    public Docket swagger(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(new ApiInfoBuilder()
+                        .title("CRUD Operation")
+                        .description("Crud Operation")
+                        .version("1.0")
+                        .build());
+    }
 }
